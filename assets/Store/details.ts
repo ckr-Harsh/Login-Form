890ab031e1abdc5a35e543bf8b17d0564c2b6ce0
Ckr Harsh <xharsh790@gmail.com>
2021-06-24 09:52:42 +0530

    Auto-generated commit

---
+++ assets/Store/details.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class CompanyDetailsModel {
+  public companyInfo: any = {
+    name: 'Cart X',
+    address: 'Nr Lakshmi Narayan Mandir, kasumpti',
+    city: 'Shimla',
+    pincode: '171009',
+    email: 'xharsh790@gmail.com',
+    phone: '7018052962',
+  };
+}
